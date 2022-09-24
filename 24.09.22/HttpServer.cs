6bd48d2ffe776b54f3cb194b32536809484e2e8f
Artur Yakupov <art.kazan@mail.ru>
2022-09-24 19:31:39 +0300

    [add] Created simpliest implementation of webserver

---
+++ 24.09.22/HttpServer.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using System.Threading;
+
+namespace HTTPServer
+{
+    internal class HttpServer
+    {
+        Dictionary<string, string> paths { get; init; }
+        HttpListener listener = new HttpListener();
+        ILogger logger;
+        int _port { get; init; }
+
+        public HttpServer(int port, Dictionary<string,string> paths, ILogger logger)
+        {
+            listener.Prefixes.Add($"http://*:{port}/");
+            this.logger = logger;
+            _port = port;
+            this.paths = paths;
+        }
+
+
+        public void Start()
+        {
+            listener.Start();
+            logger.Log($"Started server at port {_port}...");
+
+            while (true)
+            {
+                try
+                {
+                    HandleRequest();
+                }
+                catch(FileNotFoundException ex)
+                {
+                    logger.ReportError(ex.Message);
+                    this.Stop();
+                    return;
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            listener.Stop();
+            logger.Log($"Closed server ar port {_port}...");
+        }
+
+        private void HandleRequest()
+        {
+            var context = listener.GetContext();
+            var request = context.Request;
+            var response = context.Response;
+
+            if (request.Url.Host != "localhost")
+                response.StatusCode = 404;
+            else if (paths.ContainsKey(request.Url.AbsolutePath))
+            {
+                var buffer = File.ReadAllBytes(paths[request.Url.AbsolutePath]);
+                response.ContentLength64 = buffer.Length;
+                var output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                output.Close();
+
+                response.StatusCode = 200;
+            }
+            else
+                response.StatusCode = 404;
+
+
+            logger.Log($"\n{request.ProtocolVersion} {request.HttpMethod} {request.Url} {response.StatusCode}\n");
+            response.Close();
+        }
+    }
+}
---
+++ 24.09.22/Program.cs
@@ -0,0 +1,65 @@
+﻿using HTTPServer;
+
+var paths = new Dictionary<string, string>();
+
+Console.WriteLine("Enter path to GoogleMainPage.html");
+var input = Console.ReadLine();
+paths.Add( "/google", input);
+
+HttpServer server = null;
+Thread thread = null;
+
+
+Console.WriteLine("ServerShell. To start server write: start <on wich port>\n");
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    var command = Console.ReadLine().Split();
+    Console.ForegroundColor= ConsoleColor.White;
+    switch (command[0])
+    {
+        case "start":
+            var port = int.Parse(command[1]);
+            server = new HttpServer(port, paths, new ConsoleLogger());
+            thread = new Thread(server.Start);
+            thread.IsBackground = true;
+            thread.Start();
+            break;
+        case "restart":
+            if(thread != null)
+                AbortThread(thread);
+            if (server == null)
+            {
+                Console.WriteLine("You have not started server!");
+                continue;
+            }
+            thread = new Thread(server.Start);
+            thread.IsBackground = true;
+            thread.Start();
+            break;
+        case "stop":
+            if(thread != null)
+                AbortThread(thread);
+            thread = null;
+            break;
+        
+        default:
+            Console.WriteLine("Unknown command");
+            Console.WriteLine("Commands: start <port>, restart, stop");
+            break;
+    }
+}
+ void AbortThread(Thread t)
+{
+    try
+    {
+        t.Abort();
+    }
+    catch
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Server process was interrupted! Server was closed.");
+        Console.ForegroundColor = ConsoleColor.White;
+        return;
+    }
+}
